023e23965d8b7ce6eba53a098874e7d9f6f3888d
tommiv <kyle.mom.is.bitch@gmail.com>
2012-02-19 21:44:53 +0400

    added allcdcovers provider prototype (waiting for api code)

---
+++ Artwork Stack/DataAccessLayer/AllCDCovers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Xml;
+using Artwork_Stack.Model;
+
+namespace Artwork_Stack.DataAccessLayer
+{
+    public class AllCDCovers : IServiceProvider
+    {
+        private string BuildQueryString(string query)
+        {
+            return string.Format(
+                "http://www.allcdcovers.com/api/search/!username!/{0}/{1}/music",
+                CalculateHash("apisecret" + query),
+                HttpUtility.UrlEncode(query, Encoding.UTF8)
+            );
+        }
+
+        private string CalculateHash(string data)
+        {
+            var provider = MD5.Create();
+            var hashbuffer = provider.ComputeHash(Encoding.Default.GetBytes(data));
+            var sb = new StringBuilder();
+            foreach (byte t in hashbuffer)
+            {
+                sb.AppendFormat("{0:x02}", t);
+            }
+            return sb.ToString();
+        }
+
+        public UnifiedResponse Search(string query)
+        {
+            string xmlstring = Tools.Http.getText(BuildQueryString(query));
+            var doc = new XmlDocument();
+            var output = new UnifiedResponse();
+            try
+            {
+                doc.LoadXml(xmlstring);
+            }
+            catch (Exception e)
+            {
+                output.Success = false;
+                output.Error = e.Message;
+                return output;
+            }
+
+            return output;
+        }
+
+        public bool GetFullsizeUrlViaCallback { get { return false; } }
+
+        public Func<object, string> GetFullsizeUrlCallback { get { return o => string.Empty; } }
+    }
+}
--- Artwork Stack/DataAccessLayer/SupportedServices.cs
+++ Artwork Stack/DataAccessLayer/SupportedServices.cs
@@ -7,16 +7,50 @@ namespace Artwork_Stack.DataAccessLayer
     {
         internal static Dictionary<Supported, ServiceContext> Providers = new Dictionary<Supported, ServiceContext>
         {
-            { Supported.Amazon,  new ServiceContext { InternalID = Supported.Amazon,  DisplayedName = "Amazon.com",  Provider = new Amazon()  } },
-            { Supported.LastFm,  new ServiceContext { InternalID = Supported.LastFm,  DisplayedName = "Last.fm",     Provider = new LastFm()  } },
-            { Supported.Discogs, new ServiceContext { InternalID = Supported.Discogs, DisplayedName = "Discogs.com", Provider = new Discogs() } }
+            {
+                Supported.Allcdcovers,
+                new ServiceContext
+                {
+                    InternalID = Supported.Allcdcovers,  
+                    DisplayedName = "AllCDCovers.com",  
+                    Provider = new AllCDCovers()
+                }
+            }, 
+            {
+                Supported.Amazon,
+                new ServiceContext
+                {
+                    InternalID = Supported.Amazon,
+                    DisplayedName = "Amazon.com",
+                    Provider = new Amazon()
+                }
+            }, 
+            {
+                Supported.LastFm,
+                new ServiceContext
+                {
+                    InternalID = Supported.LastFm,
+                    DisplayedName = "Last.fm",
+                    Provider = new LastFm()
+                }
+            }, 
+            {
+                Supported.Discogs,
+                new ServiceContext
+                {
+                    InternalID = Supported.Discogs,
+                    DisplayedName = "Discogs.com",
+                    Provider = new Discogs()
+                }
+            }
         };
 
         internal enum Supported
         {
             LastFm,
             Discogs,
-            Amazon
+            Amazon,
+            Allcdcovers
         }
     }
 }
